682de9f89e22491511ac81e4d559f7759fe196d5
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-03-17 14:08:18 -0300

    style: change font

--- src/compoments/banner/index.js
+++ src/compoments/banner/index.js
@@ -8,12 +8,15 @@ export default function Banner() {
             <section className='banner bg-cover flex justify-center'>
                     <div className='flex flex-col items-center justify-around'>
                     <Typical 
-                    steps={['Campeão mundial em SSL', 20000,'Campeão Latino-Americano em SSL', 20000,
-                    'Campeão Latino-Americano em simulação 2D',20000,
-                    'Campeão Latino-Americano em VSS', 20000, 'Campeão Nacional em VSS', 20000]}
+                    steps={['Campeão mundial em SSL', 10000,
+                    'Campeão Latino-Americano em SSL', 10000,
+                    'Campeão Nacional em VSS', 10000,
+                    'Campeão Latino-Americano em simulação 2D',10000,
+                    'Melhor campanha de um time BR em 2021',1000,
+                    'Campeão Latino-Americano em VSS', 10000]}
                     loop={Infinity}
                     wrapper="h2"
-                    className='title text-white opacity-80 text-center uppercase tracking-20'
+                    className='title text-white opacity-80 text-center uppercase tracking-widest m-2'
                 />
               </div>
             </section>
