6bdfb9838f29f845690c8c0809e078b90e0f8db2
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-03-20 13:39:44 -0300

    style: improve design

--- src/compoments/categories/index.js
+++ src/compoments/categories/index.js
@@ -8,7 +8,7 @@ import vss from '../../assets/vss.jpg'
 import { FaBaseballBall, FaBasketballBall, FaRobot, FaRoute } from "react-icons/fa"
 
 
-const text = {title:'Categories', 
+const text = {title:'Categorias', 
 subtitle:'Nos dividimos em sub-equipes que cooperam para administrar e desenvolver várias categorias diferentes.'}
 
 const cats = [{title:'Small Size League', tour:'ROBOCUP | LARC', 
@@ -18,32 +18,32 @@ img:ssl, icon:FaRobot},
 desc:'É uma categoria que tem o objetivo de desenvolver robôs autônomos que percorrem um percurso no menor tempo possível. O robô possui sensor de reflectância para poder identificar o trajeto delimitado por uma fita.',
 img:seguidorL, icon:FaRoute},
 {title:'Simulação 2D', tour:'ROBOCUP | LARC | IRON CUP',
- desc:'Duas equipes de onze agentes autônomos jogam futebol em um estádio de futebol virtual bidimensional representado por um servidor central, chamado SoccerServer. Este servidor sabe tudo sobre o jogo, ou seja, a posição atual de todos os jogadores e a bola, a física e assim por diante. O jogo conta ainda com a comunicação entre o servidor e cada agente.', 
+ desc:'Duas equipes de onze agentes autônomos jogam futebol em um estádio virtual 2D representado por um servidor central, chamado SoccerServer. Este servidor sabe tudo sobre o jogo, ou seja, a posição atual de todos os jogadores e a bola, a física e assim por diante.', 
  img:twoD, icon:FaBaseballBall},
 {title:'Very Small Size Soccer', tour:'LARC | IRON CUP', 
-desc:'É uma competição que requer desenvolvimento nas principais áreas do RobôCIn, visão computacional para detectar informações sobre a situação do jogo; inteligência artificial para planejar todos os movimentos dos robôs, para que a equipe possa marcar gols e se defender dos adversários; e robótica que envolve eletrônica e mecânica para criar agentes que precisam se mover perfeitamente em campo.', 
+desc:'É uma competição que requer desenvolvimento em visão computacional para detectar informações sobre a situação do jogo; IA para planejar todos os movimentos dos robôs; e robótica para criar agentes que precisam se mover perfeitamente em campo.', 
 img:vss, icon:FaBasketballBall}]
 
 const AutoplaySlider = withAutoplay(AwesomeSlider)
 
 export default function Categories(){
     return (
-        <section className='p-5 pt-10 pb-10 bg-gray-100  md:h-screen'>
-            <h1 className='title text-center pb-5 text-green-700'>{text.title}</h1>
+        <section className='p-5 pt-5 pb-10 bg-gray-100 md:h-screen'>
+            <h1 className='title text-center pb-2 text-green-700'>{text.title}</h1>
             <h2 className='subtitle text-center text-gray-600 p-2'>{text.subtitle}</h2>
             <div className="flex flex-col md:flex-row justify-around">
                 {cats.map((cat) => (
-                    <AutoplaySlider className='h-96 w-5/6 m-auto' play={true}
-                     cancelOnInteraction={false} organicArrows={false}
-                     interval={10000}>
-                    <div className="p-2 m-2">
-                        <cat.icon className="h-20 w-20 text-green-600 mx-auto m-2 animate__animated animate__infinite animate__pulse" aria-hidden="true"/>
+                    <AutoplaySlider className='h-96 w-64 m-auto mb-5 mt-5' play={true}
+                      organicArrows={false}
+                     interval={10000} infinite={true}>
+                    <div className="p-2 m-2 absolute b">
+                        <cat.icon className="h-16 w-16 text-green-500 mx-auto m-2" aria-hidden="true"/>
                         <h2
-                        className="subtitle text-center m-5">
+                        className="text-center text-green-500 m-2 uppercase">
                         {cat.title}
                         </h2>
-                        <h4 className='text-center text-gray-600 p-2 pt-1'>{cat.tour}</h4>
-                        <p className="text-sm max-w-xs p-2 text-justify text-center text-gray-500">{cat.desc}</p>
+                        <h4 className='text-center text-green-100 p-2 pt-1'>{cat.tour}</h4>
+                        <p className="text-sm max-w-xs p-2 text-justify text-center text-gray-400">{cat.desc}</p>
                     </div>
                     <div data-src={cat.img}>1</div>
                     </AutoplaySlider>
