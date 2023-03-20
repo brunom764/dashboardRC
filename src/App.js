d77f2bc88811a6e51122c4897ecd4d48de560abc
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-03-20 12:10:50 -0300

    feat: add categories compoment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import About from './compoments/about';
 import Banner from './compoments/banner';
+import Categories from './compoments/categories';
 import Field from './compoments/field';
 import Footer from './compoments/footer';
 import Header from './compoments/header';
@@ -12,6 +13,7 @@ function App() {
       <Banner/>
       <About/>
       <Field/>
+      <Categories/>
       <Footer/>
     </>
   );
