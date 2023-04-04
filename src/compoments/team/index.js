27ef1df09e2c5e75866b6b327677487b6162e675
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-04-04 12:40:16 -0300

    style: add shadow in hover

--- src/compoments/team/index.js
+++ src/compoments/team/index.js
@@ -1,7 +1,7 @@
 import user from '../../assets/user.png'
 const text = {
     title: 'Depoimentos dos nossos membros',
-    subtitle: 'Nosso processo seletivo está aberto, venha fazer parte do RobôCIn!'
+    subtitle: 'Nosso processo seletivo está aberto. Clique aqui e venha fazer parte do RobôCIn!'
 }
 export default function Team(){
     const members = [
@@ -16,14 +16,14 @@ export default function Team(){
         <h1 className="title titleMain">{text.title}</h1>
         <div className='flex flex-col md:flex-row justify-around xl:mt-3 p-2'>
             {members.map((member) => (
-                <div key={member.key} className='p-2 flex flex-col items-center border m-2 rounded-lg shadow-md shadow-green-500 my-5'>
+                <div key={member.key} className='bg-white px-2 py-5 flex flex-col items-center border m-2 rounded-lg shadow-md shadow-green-500 my-5 hover:shadow-lg hover:shadow-green-500'>
                     <img src={member.img} alt={member.name} className='h-32 w-32'/>
                     <h3 className="text-center text-green-600 m-1.5 uppercase">{member.name}</h3>
                     <p className="text-sm max-w-xs p-1.5 text-justify text-center text-gray-400">{member.message}</p>
                 </div>
             ))}
         </div>
-        <a href='https://robocin.com.br/seletiva.html'><h2 className="subtitle subtitleMain mt-5 border-b-2 border-green-600">{text.subtitle}</h2></a>
+        <a href='https://robocin.com.br/seletiva.html'><h2 className="subtitle subtitleMain mt-5 border-green-600">{text.subtitle}</h2></a>
         </section>
     );
 };
