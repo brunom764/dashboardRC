ad2fa87ba0902b1298e00ab87e5516dc3b47eb80
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-04-04 14:59:00 -0300

    fix: fix bug in header

--- src/compoments/header/index.js
+++ src/compoments/header/index.js
@@ -108,6 +108,15 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-green-700 
         px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#home" className="-m-1.5 p-1.5">
+              <span className="sr-only">RoboCin</span>
+              <img
+                className="h-8 w-auto hidden"
+                src={logoW}
+                alt="logo"
+              />
+            </a>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-white"
@@ -116,6 +125,7 @@ export default function Header() {
               <span className="sr-only">Close menu</span>
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
+          </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
