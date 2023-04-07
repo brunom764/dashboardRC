aa6ce5f7629e13a4ebf2201d1f63d1b34fd80578
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-04-07 10:53:44 -0300

    Style: change in depoiments cards

--- src/compoments/team/index.js
+++ src/compoments/team/index.js
@@ -1,13 +1,19 @@
-import user from '../../assets/user.png'
+import avatar1 from '../../assets/avatar/avatar1.png'
+import avatar2 from '../../assets/avatar/avatar2.png'
+import avatar3 from '../../assets/avatar/avatar3.png'
+
 const text = {
     title: 'Depoimentos dos nossos membros',
     subtitle: 'Nosso processo seletivo está aberto. Clique aqui e venha fazer parte do RobôCIn!'
 }
 export default function Team(){
     const members = [
-        { key:0, name: 'João', img:user, pos:'Desenvolvimento', message:'Ser membro do RobôCIn tem me permitido aplicar todo o conhecimento adquirido na faculdade em projetos reais, o que tem sido um grande diferencial na minha formação e um grande passo para minha carreira.' },
-        { key:1, name: 'Maria', img:user, pos:'Administração', message: 'O RobôCIn me proporcionou não apenas a chance de trabalhar em projetos empolgantes, mas também de conhecer pessoas de diversas áreas e culturas, o que tem sido uma experiência enriquecedora para mim.'},
-        { key:2, name: 'Pedro', img:user, pos:'Comunicação', message: 'Fazer parte do RobôCIn tem sido uma experiência incrível, não apenas por ter a oportunidade de desenvolver habilidades, mas também por ter a chance de trabalhar com um grupo de pessoas incríveis e dedicadas.'},
+        { key:0, name: 'Ana Júlia', img:avatar1, pos:'Desenvolvedora VSS', title:'aplicar o conhecimento adquirido na faculdade em projetos reais',
+         message:'Ser membro do RobôCIn tem me permitido aplicar todo o conhecimento adquirido na faculdade em projetos reais, o que tem sido um grande diferencial na minha formação e um grande passo para minha carreira.' },
+        { key:1, name: 'Geydson Renan', img:avatar2, pos:'Simulação 2D', title:'conhecer pessoas de diversas áreas e culturas',
+          message: 'O RobôCIn me proporcionou não apenas a chance de trabalhar em projetos empolgantes, mas também de conhecer pessoas de diversas áreas e culturas, o que tem sido uma experiência enriquecedora para mim.'},
+        { key:2, name: 'Maria Beatriz', img:avatar3, pos:'Comunicação', title:'oportunidade de desenvolver habilidades',
+          message: 'Fazer parte do RobôCIn tem sido uma experiência incrível, não apenas por ter a oportunidade de desenvolver habilidades, mas também por ter a chance de trabalhar com um grupo de pessoas incríveis e dedicadas.'},
     ];
 
     return (
@@ -15,16 +21,19 @@ export default function Team(){
         <h1 className="title titleMain">{text.title}</h1>
         <div className='flex flex-col md:flex-row justify-around xl:mt-3 p-2'>
             {members.map((member) => (
-                <div key={member.key} className='bg-white px-2 py-5 flex flex-col items-center m-2 rounded-lg my-3 border shadow-lg hover:shadow-xl'>
-                    <div className='flex items-center justify-around'>
-                        <img src={member.img} alt={member.name} className='h-32 w-32 mr-2'/>
-                        <div className='flex flex-col ml-2'>
-                            <h3 className="text-center text-green-600 m-1.5 uppercase">{member.name}</h3>
-                            <h6 className="text-center text-green-500 mb-1.5">{member.pos}</h6>
+                <figure key={member.key} className="flex flex-col items-center justify-center p-6 m-2 text-center bg-white border-b border-gray-200 md:border-r">
+                    <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8">
+                        <h3 className="text-lg font-semibold text-gray-900 uppercase">{member.title}</h3>
+                        <p className="my-4">"{member.message}"</p>
+                    </blockquote>
+                    <figcaption className="flex items-center justify-center space-x-3">
+                        <img className="rounded-full w-9 h-9" src={member.img} alt={member.name}/>
+                        <div className="space-y-0.5 font-medium text-left">
+                            <div>{member.name}</div>
+                            <div className="text-sm text-gray-500 dark:text-gray-400">{member.pos}</div>
                         </div>
-                    </div>
-                    <p className="text-sm max-w-xs p-1.5 text-justify text-center text-gray-400">"{member.message} "</p>
-                </div>
+                    </figcaption>    
+                </figure>
             ))}
         </div>
         <a href='https://robocin.com.br/seletiva.html'><h2 className="subtitle subtitleMain mt-5 border-green-600 my-2">{text.subtitle}</h2></a>
