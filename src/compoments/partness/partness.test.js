d1df8694489d123c1889c48e3754f30a73d1629f
Bruno Miguel Moreira Albuquerque <brunom764@gmail.com>
2023-04-15 09:41:05 -0300

    test: update snapshots

--- src/compoments/partness/partness.test.js
+++ src/compoments/partness/partness.test.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import Partness from './index';
+import { render } from "@testing-library/react"
 
-describe('Partness component', () => {
-  it('renders all images', () => {
-    render(<Partness />);
-    const cinImg = screen.getByAltText('CIn');
-    const cesarImg = screen.getByAltText('Cesar School');
-    const hsbsImg = screen.getByAltText('HSBS');
-    const microsoftImg = screen.getByAltText('Microsoft');
-    const mouraImg = screen.getByAltText('Moura');
-    const veroliImg = screen.getByAltText('Veroli');
+import Partness from "./index"
 
-    expect(cinImg).toBeInTheDocument();
-    expect(cesarImg).toBeInTheDocument();
-    expect(hsbsImg).toBeInTheDocument();
-    expect(microsoftImg).toBeInTheDocument();
-    expect(mouraImg).toBeInTheDocument();
-    expect(veroliImg).toBeInTheDocument();
-  });
-});
+describe("<Partness />", () => {
+  it("should match snapshot", () => {
+    const { container } = render(<Partness />)
 
+    expect(container).toMatchSnapshot()
+  })
+})
--- src/compoments/tv/tv.test.js
+++ src/compoments/tv/tv.test.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, waitFor, screen , act} from '@testing-library/react';
+import { render, waitFor, screen} from '@testing-library/react';
 import TV from './index';
 
 
 test('renders TV component with videos', async () => {
   render(<TV />);
   await waitFor(() => {
-    act(() => {
       expect(screen.getByText('Ep. 1 | Mini Doc. RobôCIn')).toBeInTheDocument();
-    });
   });
   await waitFor(() => {
-    act(() => {
       expect(screen.getByText('Ep. 2 | Mini Doc. RobôCIn')).toBeInTheDocument();
     });
-  });
 });
 
 
